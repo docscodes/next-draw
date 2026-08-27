e9a255401e4a7a9c2d01150d227e66f7744b9f41
docscodes <docscodes@gmail.com>
2026-08-27 17:14:31 -0400

    layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,12 @@
 import { Geist, Geist_Mono } from "next/font/google"
 
-import "./globals.css"
+import AppSidebar from "@/components/AppSidebar"
+import Navbar from "@/components/Navbar"
 import { ThemeProvider } from "@/components/theme-provider"
-import { cn } from "@/lib/utils";
+import { cn } from "@/lib/utils"
+import "./globals.css"
 
-const geist = Geist({subsets:['latin'],variable:'--font-sans'})
+const geist = Geist({ subsets: ["latin"], variable: "--font-sans" })
 
 const fontMono = Geist_Mono({
   subsets: ["latin"],
@@ -20,10 +22,21 @@ export default function RootLayout({
     <html
       lang="en"
       suppressHydrationWarning
-      className={cn("antialiased", fontMono.variable, "font-sans", geist.variable)}
+      className={cn(
+        "antialiased",
+        fontMono.variable,
+        "font-sans",
+        geist.variable
+      )}
     >
       <body>
-        <ThemeProvider>{children}</ThemeProvider>
+        <ThemeProvider>
+          <AppSidebar />
+          <main className="w-full">
+            <Navbar />
+            <div className="px-4">{children}</div>
+          </main>
+        </ThemeProvider>
       </body>
     </html>
   )
